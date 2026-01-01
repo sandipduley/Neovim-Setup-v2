909593d089448b3f1593f8e0edc8c39bdd566d81
sandipduley <sandipp0987rahul@gmail.com>
2026-01-01 12:19:19 +0000

    FIX: colors -- lualine and bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,78 +1,144 @@
 return {
-  'akinsho/bufferline.nvim',
-  dependencies = {
-    'moll/vim-bbye',               -- close buffers without closing window
-    'nvim-tree/nvim-web-devicons', -- filetype icons
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = {
+		"moll/vim-bbye", -- close buffers without closing window
+		"nvim-tree/nvim-web-devicons", -- filetype icons
+	},
 
-  config = function()
-    local bufferline = require('bufferline')
+	config = function()
+		local bufferline = require("bufferline")
 
-    bufferline.setup {
-      options = {
-        mode = 'buffers',
-        themable = true,
-        numbers = 'none',
+		bufferline.setup({
+			options = {
+				mode = "buffers",
+				themable = true,
+				numbers = "none",
 
-        close_command = 'Bdelete! %d',
-        right_mouse_command = 'Bdelete! %d',
-        left_mouse_command = 'buffer %d',
-        middle_mouse_command = nil,
+				close_command = "Bdelete! %d",
+				right_mouse_command = "Bdelete! %d",
+				left_mouse_command = "buffer %d",
+				middle_mouse_command = nil,
 
-        buffer_close_icon = '',
-        close_icon = '󰯇',
+				buffer_close_icon = "",
+				close_icon = "󰯇",
 
-        path_components = 1,
-        modified_icon = '●',
-        left_trunc_marker = '',
-        right_trunc_marker = '',
+				path_components = 1,
+				modified_icon = "●",
+				left_trunc_marker = "",
+				right_trunc_marker = "",
 
-        max_name_length = 30,
-        max_prefix_length = 30,
-        tab_size = 21,
+				max_name_length = 30,
+				max_prefix_length = 30,
+				tab_size = 21,
 
-        diagnostics = false,
-        diagnostics_update_in_insert = false,
+				diagnostics = false,
+				diagnostics_update_in_insert = false,
 
-        color_icons = true,
-        show_buffer_icons = true,
-        show_buffer_close_icons = true,
-        show_close_icon = true,
-        persist_buffer_sort = true,
+				color_icons = true,
+				show_buffer_icons = true,
+				show_buffer_close_icons = true,
+				show_close_icon = true,
+				persist_buffer_sort = true,
 
-        separator_style = { '│', '│' },
-        enforce_regular_tabs = true,
-        always_show_bufferline = true,
-        show_tab_indicators = false,
+				separator_style = { "│", "│" },
+				enforce_regular_tabs = true,
+				always_show_bufferline = true,
+				show_tab_indicators = false,
 
-        indicator = { style = 'underline' },
-        icon_pinned = '󰐃',
+				indicator = { style = "underline" },
+				icon_pinned = "󰐃",
 
-        minimum_padding = 1,
-        maximum_padding = 5,
-        maximum_length = 15,
+				minimum_padding = 1,
+				maximum_padding = 5,
+				maximum_length = 15,
 
-        sort_by = 'insert_at_end',
-      },
+				sort_by = "insert_at_end",
+			},
 
-      highlights = {
-        separator = { fg = '#434C5E' },
-        buffer_selected = { bold = true, italic = false },
-      },
-    }
+			highlights = {
+				-- Background
+				fill = {
+					bg = "NONE",
+				},
 
-    local opts = { noremap = true, silent = true, desc = 'Go to Buffer' }
+				-- Buffers
+				background = {
+					fg = "#6B7280",
+					bg = "NONE",
+				},
 
-    -- Cycle buffers
-    vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', opts)
-    vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', opts)
+				buffer_visible = {
+					fg = "#A5B4FC",
+					bg = "NONE",
+				},
 
-    -- Jump to specific buffers
-    for i = 1, 9 do
-      vim.keymap.set('n', '<leader>' .. i, string.format(
-        "<cmd>lua require('bufferline').go_to_buffer(%d)<CR>", i
-      ), opts)
-    end
-  end,
+				buffer_selected = {
+					fg = "#ECEFF4",
+					bg = "NONE",
+					bold = true,
+					italic = false,
+				},
+
+				-- Separator
+				separator = {
+					fg = "#3B4252",
+					bg = "NONE",
+				},
+
+				separator_visible = {
+					fg = "#4C566A",
+					bg = "NONE",
+				},
+
+				separator_selected = {
+					fg = "#81A1C1",
+					bg = "NONE",
+				},
+
+				-- Modified
+				modified = {
+					fg = "#EBCB8B",
+					bg = "NONE",
+				},
+
+				modified_selected = {
+					fg = "#EBCB8B",
+					bg = "NONE",
+				},
+
+				-- Close buttons
+				close_button = {
+					fg = "#6B7280",
+					bg = "NONE",
+				},
+
+				close_button_selected = {
+					fg = "#BF616A",
+					bg = "NONE",
+				},
+
+				-- Indicators
+				indicator_selected = {
+					fg = "#88C0D0",
+					bg = "NONE",
+				},
+			},
+		})
+
+		local opts = { noremap = true, silent = true, desc = "Go to Buffer" }
+
+		-- Cycle buffers
+		vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", opts)
+		vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", opts)
+
+		-- Jump to specific buffers
+		for i = 1, 9 do
+			vim.keymap.set(
+				"n",
+				"<leader>" .. i,
+				string.format("<cmd>lua require('bufferline').go_to_buffer(%d)<CR>", i),
+				opts
+			)
+		end
+	end,
 }
-
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,107 +1,113 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  config = function()
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		-- Colors & Theme
 
-    -- Colors & Theme
+		local colors = {
+			bg = "#2a2f45",
+			fg = "#f0f0f0",
 
-    local colors = {
-      -- Base
-      bg    = '#1f2229',
-      fg    = '#d0d0d0',
+			red = "#ff859f",
+			orange = "#ffb987",
+			yellow = "#f6d98a",
+			green = "#17E68A",
+			blue = "#8ab4ff",
+			purple = "#dd9cf0",
+			cyan = "#7ee6ed",
 
-      -- Accents
-      red    = '#f7768e',
-      orange = '#ff9e64',
-      yellow = '#e0af68',
-      green  = '#9ece6a',
-      blue   = '#61afef',
-      purple = '#c678dd',
-      cyan   = '#56b6c2',
+			gray1 = "#b0b6c9",
+			gray2 = "#343a55",
+			gray3 = "#424a6b",
+		}
 
-      -- Grays
-      gray1 = '#7f8490',
-      gray2 = '#2c303c',
-      gray3 = '#3a3f4b',}
+		local custom_theme = {
+			normal = {
+				a = { fg = colors.bg, bg = colors.green, gui = "bold" },
+				b = { fg = colors.fg, bg = colors.gray3 },
+				c = { fg = colors.fg, bg = colors.gray2 },
+			},
+			insert = { a = { fg = colors.bg, bg = colors.blue, gui = "bold" } },
+			visual = { a = { fg = colors.bg, bg = colors.purple, gui = "bold" } },
+			replace = { a = { fg = colors.bg, bg = colors.red, gui = "bold" } },
+			terminal = { a = { fg = colors.bg, bg = colors.cyan, gui = "bold" } },
+			command = { a = { fg = colors.bg, bg = colors.orange, gui = "bold" } },
+			inactive = {
+				a = { fg = colors.gray1, bg = colors.bg, gui = "bold" },
+				b = { fg = colors.gray1, bg = colors.bg },
+				c = { fg = colors.gray1, bg = colors.gray2 },
+			},
+		}
 
-    local custom_theme = {
-      normal   = { a = { fg = colors.bg, bg = colors.green, gui = 'bold' }, b = { fg = colors.fg, bg = colors.gray3 }, c = { fg = colors.fg, bg = colors.gray2 } },
-      insert   = { a = { fg = colors.bg, bg = colors.blue, gui = 'bold' } },
-      visual   = { a = { fg = colors.bg, bg = colors.purple, gui = 'bold' } },
-      replace  = { a = { fg = colors.bg, bg = colors.red, gui = 'bold' } },
-      terminal = { a = { fg = colors.bg, bg = colors.cyan, gui = 'bold' } },
-      command  = { a = { fg = colors.bg, bg = colors.orange, gui = 'bold' } },
-      inactive = { a = { fg = colors.gray1, bg = colors.bg, gui = 'bold' }, b = { fg = colors.gray1, bg = colors.bg }, c = { fg = colors.gray1, bg = colors.gray2 } },
-    }
+		local theme_choice = os.getenv("NVIM_THEME") or "custom"
 
-    local theme_choice = os.getenv('NVIM_THEME') or 'custom'
+		-- Helper Functions
 
+		local visible_if_wide = function()
+			return vim.fn.winwidth(0) > 100
+		end
 
-    -- Helper Functions
+		-- Components
 
-    local visible_if_wide = function()
-      return vim.fn.winwidth(0) > 100
-    end
+		local mode_component = {
+			"mode",
+			fmt = function(str)
+				return visible_if_wide() and (" " .. str) or (" " .. str:sub(1, 1))
+			end,
+		}
 
+		local file_component = { "filename", file_status = true, path = 1 }
 
-    -- Components
+		local diagnostic_component = {
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			sections = { "error", "warn" },
+			symbols = { error = " ", warn = " ", info = " ", hint = " " },
+			colored = true,
+			update_in_insert = true,
+			cond = visible_if_wide,
+		}
 
-    local mode_component = {
-      'mode',
-      fmt = function(str)
-        return visible_if_wide() and (' ' .. str) or (' ' .. str:sub(1,1))
-      end,
-    }
+		local diff_component = {
+			"diff",
+			colored = true,
+			symbols = { added = " ", modified = "柳 ", removed = " " },
+			cond = visible_if_wide,
+		}
 
-    local file_component = { 'filename', file_status = true, path = 1 }
+		-- Lualine Setup
 
-    local diagnostic_component = {
-      'diagnostics',
-      sources = { 'nvim_diagnostic' },
-      sections = { 'error', 'warn' },
-      symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
-      colored = true,
-      update_in_insert = true,
-      cond = visible_if_wide,
-    }
-
-    local diff_component = {
-      'diff',
-      colored = true,
-      symbols = { added = ' ', modified = '柳 ', removed = ' ' },
-      cond = visible_if_wide,
-    }
-
-
-    -- Lualine Setup
-
-    require('lualine').setup {
-      options = {
-        icons_enabled = true,
-        theme = theme_choice == 'custom' and custom_theme or theme_choice,
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-        disabled_filetypes = { 'alpha', 'neo-tree', 'Avante' },
-        always_divide_middle = true,
-      },
-      sections = {
-        lualine_a = { mode_component },
-        lualine_b = { 'branch' },
-        lualine_c = { file_component },
-        lualine_x = { diagnostic_component, diff_component, { 'encoding', cond = visible_if_wide }, { 'filetype', cond = visible_if_wide } },
-        lualine_y = { 'location' },
-        lualine_z = { 'progress' },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { file_component },
-        lualine_x = { { 'location', padding = 0 } },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = { 'fugitive' },
-    }
-  end,
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = theme_choice == "custom" and custom_theme or theme_choice,
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				disabled_filetypes = { "alpha", "neo-tree", "Avante" },
+				always_divide_middle = true,
+			},
+			sections = {
+				lualine_a = { mode_component },
+				lualine_b = { "branch" },
+				lualine_c = { file_component },
+				lualine_x = {
+					diagnostic_component,
+					diff_component,
+					{ "encoding", cond = visible_if_wide },
+					{ "filetype", cond = visible_if_wide },
+				},
+				lualine_y = { "location" },
+				lualine_z = { "progress" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { file_component },
+				lualine_x = { { "location", padding = 0 } },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			extensions = { "fugitive" },
+		})
+	end,
 }
-
